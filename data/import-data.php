e28bc3b3ca86f3910cb3f5486c06e90f20d13544
mharley87 <mharley87@hotmail.com>
2012-03-23 15:20:35 -0400

    commit to github. added google maps, geolocations, and rating system

--- data/import-data.php
+++ data/import-data.php
@@ -5,8 +5,8 @@ require_once '../includes/db.php';
 $places_xml = simplexml_load_file('2009_ball_diamonds.kml');
 
 $sql = $db->prepare('
-		INSERT INTO locations (name, longitude, latitude)
-		VALUES (:name, :longitude, :latitude)
+		INSERT INTO locations (name, longitude, latitude, adr, rate_count, rate_total)
+		VALUES (:name, :longitude, :latitude, :adr, :rate_count, rate_total)
 ');
 
 foreach ($places_xml->Document->Folder[0]->Placemark as $place) {
@@ -28,6 +28,9 @@ foreach ($places_xml->Document->Folder[0]->Placemark as $place) {
 
 
 $sql->bindValue(':name', $place->name, PDO::PARAM_STR);
+$sql->bindValue(':rate_count', $place->rate_count, PDO::PARAM_STR);
+$sql->bindValue(':rate_total', $place->rate_total, PDO::PARAM_STR);
+$sql->bindValue(':adr', $place->adr, PDO::PARAM_STR);
 $sql->bindValue(':longitude', $coords[0], PDO::PARAM_STR);
 $sql->bindValue(':latitude', $coords[1], PDO::PARAM_STR);
 $sql->execute();
